07ae63960668acd28470401c3121c25412a9a86e
Khushi Khari <144012000+khushikhari@users.noreply.github.com>
2023-09-07 18:46:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('dashboard.html', (req, res) => {
+    // Validate user's credentials (replace this with actual validation)
+    if (req.body.username === 'user' && req.body.password === 'password') {
+        req.session.loggedIn = true;
+        res.redirect('dashboard.html');
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('dashboard.html', (req, res) => {
+    if (req.session.loggedIn) {
+        res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
